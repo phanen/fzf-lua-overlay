cde0f95b87f3516189c4485337ea2adaf4f36565
phanium <nxhsdu@qq.com>
2024-03-30 19:00:31 +0800

    fix: error path

--- lua/fzf-lua-overlay/config.lua
+++ lua/fzf-lua-overlay/config.lua
@@ -4,9 +4,9 @@ local default = {
   notes_history = vim.fn.stdpath 'state' .. '/fzf_notes_history',
   -- stylua: ignore
   notes_actions = {
-    ['ctrl-g'] = function(...) require('lua.fzf-lua-overlay.actions').toggle_daily(...) end,
-    ['ctrl-n'] = function(...) require('lua.fzf-lua-overlay.actions').create_notes(...) end,
-    ['ctrl-x'] = function(...) require('lua.fzf-lua-overlay.actions').delete_files(...) end,
+    ['ctrl-g'] = function(...) require('fzf-lua-overlay.actions').toggle_daily(...) end,
+    ['ctrl-n'] = function(...) require('fzf-lua-overlay.actions').create_notes(...) end,
+    ['ctrl-x'] = function(...) require('fzf-lua-overlay.actions').delete_files(...) end,
   },
 }
 
