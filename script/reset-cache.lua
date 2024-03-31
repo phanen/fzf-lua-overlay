6a7849ee84fe5b37e618ea82c7613f3264f0051a
phanium <nxhsdu@qq.com>
2024-03-31 14:00:18 +0800

    chore: debug script

---
+++ script/reset-cache.lua
@@ -0,0 +1,6 @@
+#/bin/nvim -l
+package.path = package.path .. ';lua/?.lua'
+local cache_dir = require('fzf-lua-overlay.config').opts.cache_dir
+require('fzf-lua-overlay.util').ls(cache_dir, function(path, _, _)
+  vim.uv.fs_unlink(path)
+end)
