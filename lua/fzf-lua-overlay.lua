a9e0656a58c23c53b21c3b735930e2d6804f5f91
phanium <nxhsdu@qq.com>
2024-04-17 16:52:49 +0800

    feat: passthrough opts

--- lua/fzf-lua-overlay.lua
+++ lua/fzf-lua-overlay.lua
@@ -9,12 +9,20 @@ M.setup = function(opts) require('fzf-lua-overlay.config').setup(opts) end
 
 return setmetatable(M, {
   __index = function(_, k)
-    return function()
+    return function(_opts)
       local o = require('fzf-lua-overlay.overlay')[k]
       local name, opts, fzf_exec_arg = o.name, o.opts, o.fzf_exec_arg
       opts = vim.tbl_deep_extend('force', opts, opts_fn(k) or {})
-      local args = name == 'fzf_exec' and vim.F.pack_len(fzf_exec_arg, opts) or vim.F.pack_len(opts)
-      require('fzf-lua')[name](vim.F.unpack_len(args))
+
+      -- backend of new pickers (no use as api)
+      if name == 'fzf_exec' then
+        require('fzf-lua').fzf_exec(fzf_exec_arg, opts)
+      else
+        opts = vim.tbl_deep_extend('force', opts, _opts or {})
+        require('fzf-lua')[name](opts)
+      end
+      -- local args = name == 'fzf_exec' and vim.F.pack_len(fzf_exec_arg, opts) or vim.F.pack_len(opts)
+      -- require('fzf-lua')[name](vim.F.unpack_len(args))
     end
   end,
 })
