4a1c75785ccc748c60a99b4a1affca476bdcf67e
phanium <nxhsdu@qq.com>
2024-09-06 17:10:49 +0800

    fix(recent): buf/closed/shada

--- lua/flo/providers/recentfiles.lua
+++ lua/flo/providers/recentfiles.lua
@@ -19,25 +19,41 @@ M.fn = function(opts)
         if err then fzf_cb() end
       end)
     end
+
     coroutine.wrap(function()
       local utils = require 'fzf-lua.utils'
       local co = coroutine.running()
 
-      local buflist = vim.fn.getbufinfo { bufloaded = 1, buflisted = 1 }
-      local bufmap = {}
-      -- get table of values from list of tables
-      for _, buf in ipairs(buflist) do
-        bufmap[buf.name] = true
-      end
+      local bufnr = vim.api.nvim_get_current_buf()
+      local curr_file = vim.api.nvim_buf_get_name(bufnr)
+
+      local stat_fn = not opts.stat_file and function(_) return true end
+        or type(opts.stat_file) == 'function' and opts.stat_file
+        or function(file)
+          local stat = vim.uv.fs_stat(file)
+          return (
+            not utils.path_is_directory(file, stat)
+            -- FIFO blocks `fs_open` indefinitely (#908)
+            and not utils.file_is_fifo(file, stat)
+            and utils.file_is_readable(file)
+          )
+        end
+
+      -- local buflist = vim.fn.getbufinfo { bufloaded = 1, buflisted = 1 }
+      -- local bufmap = {}
+      -- for _, buf in ipairs(buflist) do
+      --   bufmap[buf.name] = true
+      -- end
 
       M._lru.foreach(function(file)
-        local fs_stat = not utils.file_is_fifo(file) and utils.file_is_readable(file)
-        if fs_stat and not bufmap[file] then add_entry(file, co) end
+        if stat_fn(file) and file ~= curr_file then add_entry(file, co) end
       end)
-      for _, file in ipairs(vim.v.oldfiles) do
-        local fs_stat = not utils.file_is_fifo(file) and utils.file_is_readable(file)
-        if fs_stat and not sess_files[file] and not bufmap[file] then add_entry(file, co) end
-      end
+      vim
+        .iter(vim.v.oldfiles)
+        :filter(stat_fn)
+        :filter(function(file) return file ~= curr_file end)
+        :filter(function(file) return not sess_files[file] end)
+        :each(function(file) add_entry(file, co) end)
       fzf_cb()
     end)()
   end, opts)
